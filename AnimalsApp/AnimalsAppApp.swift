36645268f89fcdc4f5431b70520ab31298956ff9
IndritSaveta <indrit.saveta@10clouds.com>
2023-01-16 12:34:10 +0100

    Initial Commit

---
+++ AnimalsApp/AnimalsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalsAppApp.swift
+//  AnimalsApp
+//
+//  Created by Indrit Saveta on 16.1.23.
+//
+
+import SwiftUI
+
+@main
+struct AnimalsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
